ea801e75b830556b255fc6cb1cd4351dd84e83d4
CodeBackpackofEndika <32270483+CodeBackpackofEndika@users.noreply.github.com>
2017-12-11 13:36:44 +0100

    Personalización propia.

--- js/usuarios.js
+++ js/usuarios.js
@@ -4,26 +4,26 @@ $(function () {
         $(this).css({
             "display": "none"
         });
-        var usuarios=$.ajax({
-            method:'GET',
+        var usuarios = $.ajax({
+            method: 'GET',
             url: 'https://randomuser.me/api/',
-            data:'results=50'
+            data: 'results=50'
 
         });
         usuarios.done(function (data) {
             console.log(data);
-            $(data.results).each(function (iE,vE) {
-                    console.log("hastaaquífunciona");
+            $(data.results).each(function (iE, vE) {
+                console.log("hastaaquífunciona");
                 $("#listusuarios").append("<div class='row text-left'><img src='"
-                    +vE.picture.large+"'></img><br>"
-                    +vE.name.first+" "
-                    +vE.name.last+"<br>"
-                    +vE.email+"<br>"
-                    +vE.location.street+", "
-                    +vE.location.postcode+", "
-                    +vE.location.city+"("
-                    +vE.location.state
-                    +")</div>")
+                    + vE.picture.large + "'></img><br>"
+                    + vE.name.first + " "
+                    + vE.name.last + "<br>"
+                    + vE.email + "<br>"
+                    + vE.location.street + ", "
+                    + vE.location.postcode + ", "
+                    + vE.location.city + "("
+                    + vE.location.state
+                    + ")</div>")
             })
         });
         usuarios.fail(function () {
